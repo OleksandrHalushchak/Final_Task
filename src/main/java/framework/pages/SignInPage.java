4cef2c74f40719d87fb2cb2195517de9766afee7
olegdatsiuk <oleg.datsiuk@getoxygen.com>
2023-02-02 20:59:46 +0200

    init commit

---
+++ src/main/java/framework/pages/SignInPage.java
@@ -0,0 +1,33 @@
+package framework.pages;
+
+import org.openqa.selenium.By;
+
+public class SignInPage extends BasePage {
+
+  private final By emailFieldLocator = By.id("email");
+  private final By passwordFieldLocator = By.id("pass");
+  private final By signInButtonLocator = By.xpath(
+      "//button[@id='send2' and contains(@class,'primary')]");
+  private final By errorMessageLocator = By.xpath("//div[@data-ui-id='message-error']/div");
+
+
+  public SignInPage enterEmail(String email) {
+    find(emailFieldLocator).sendKeys(email);
+    return this;
+  }
+
+  public SignInPage enterPassword(String password) {
+    find(passwordFieldLocator).sendKeys(password);
+    return this;
+  }
+
+  public SignInPage clickSignInButtonWithExpectedFail() {
+    find(signInButtonLocator).click();
+    return this;
+  }
+
+  public String getErrorMessage() {
+    return find(errorMessageLocator).getText();
+  }
+
+}
---
+++ src/main/java/framework/pages/components/ProductComponent.java
@@ -0,0 +1,35 @@
+package framework.pages.components;
+
+import lombok.Getter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+@Getter
+public class ProductComponent {
+
+  private WebElement image;
+  private WebElement nameWe;
+  private String name;
+  private String rating;
+  private String price;
+  private String reviews;
+
+  public ProductComponent(WebElement container) {
+    this.image = container.findElement(By.xpath(".//img[@class='product-image-photo']"));
+    this.nameWe = container.findElement(By.xpath(".//a[@class='product-item-link']"));
+    this.name = nameWe.getText();
+    try {
+      this.rating = container.findElement(By.xpath(".//div[@class='rating-result']"))
+          .getAttribute("title");
+    } catch (NoSuchElementException e) {
+      this.rating = null;
+    }
+    this.price = container.findElement(By.xpath(".//span[@class='price']")).getText();
+    try {
+      this.reviews = container.findElement(By.xpath(".//a[@class='action view']")).getText();
+    } catch (NoSuchElementException e) {
+      this.reviews = null;
+    }
+  }
+}
